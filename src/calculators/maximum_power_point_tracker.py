3ed154cbbd1c85c47980c472ba9932eae97032ed
Guy Keogh <guykeogh@proton.me>
2023-02-23 05:07:38 +0000

    Cache calculations

--- src/calculators/maximum_power_point_tracker.py
+++ src/calculators/maximum_power_point_tracker.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from functools import lru_cache
 from typing import Final
 
 import numpy as np
@@ -8,16 +9,14 @@ from src.calculators.total_power_output import TotalPowerOutput
 
 
 class MaximumPowerPointTracker:
+    cache: dict[tuple[u.Quantity, u.Quantity, u.Quantity, u.Quantity]] = dict()
+
     def __init__(
         self,
         t_sky: u.Quantity,
         t_cell: u.Quantity,
         E_g: u.Quantity,
     ):
-        self.t_sky: Final[u.Quantity] = t_sky
-        self.t_cell: Final[u.Quantity] = t_cell
-        self.E_g: Final[u.Quantity] = E_g
-
         voltage_to_power_dict: dict[Decimal, u.Quantity] = dict()
         voltage_range: Final[np.ndarray] = np.arange(
             start=-0.04, stop=-0.01, step=0.001
@@ -28,21 +27,51 @@ class MaximumPowerPointTracker:
             voltage_to_power_dict[
                 voltage_decimal
             ] = self._calculate_power_output_for_voltage(
-                voltage=(voltage_decimal * u.volt)
+                voltage=(voltage_decimal * u.volt),
+                E_g=E_g,
+                t_cell=t_cell,
+                t_sky=t_sky,
             )
+
+            print(self._calculate_power_output_for_voltage.cache_info())
 
         self.voltage_to_power_dict: Final[
             dict[Decimal, u.Quantity]
         ] = voltage_to_power_dict
 
-    def _calculate_power_output_for_voltage(self, voltage: u.Quantity) -> u.Quantity:
-        chemical_potential_driving_emission: Final[u.Quantity] = voltage.value * u.eV
-        return TotalPowerOutput(E_g=self.E_g).get_total_power_output(
-            voltage=voltage,
-            t_sky=self.t_sky,
-            t_cell=self.t_cell,
-            chemical_potential_driving_emission=chemical_potential_driving_emission,
+    @classmethod
+    @lru_cache(maxsize=None)
+    def _calculate_power_output_for_voltage(
+        self,
+        voltage: u.Quantity,
+        E_g: u.Quantity,
+        t_sky: u.Quantity,
+        t_cell: u.Quantity,
+    ) -> u.Quantity:
+        voltage = round(voltage.value, 3) * u.volt
+        t_sky = round(t_sky.value) * u.Kelvin
+        t_cell = round(t_cell.value) * u.Kelvin
+
+        cache_lookup: tuple[u.Quantity, u.Quantity, u.Quantity, u.Quantity] = (
+            voltage,
+            E_g,
+            t_sky,
+            t_cell,
         )
+        if cache_lookup not in self.cache:
+            print("Miss!")
+            chemical_potential_driving_emission: Final[u.Quantity] = (
+                voltage.value * u.eV
+            )
+            return TotalPowerOutput(E_g=E_g).get_total_power_output(
+                voltage=voltage,
+                t_sky=t_sky,
+                t_cell=t_cell,
+                chemical_potential_driving_emission=chemical_potential_driving_emission,
+            )
+        else:
+            print("Cache hit!")
+            return self.cache[cache_lookup]
 
     def get_max_power(self) -> u.Quantity:
         return max(self.voltage_to_power_dict.values()).value * (u.watt / u.meter**2)
