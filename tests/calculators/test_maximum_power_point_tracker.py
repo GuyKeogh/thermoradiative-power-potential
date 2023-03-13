5c0a5af4c58201ce51fd77f575c71488d3e8d92b
Guy Keogh <guykeogh@proton.me>
2023-03-13 02:08:51 +0000

    Fix test

--- tests/calculators/test_maximum_power_point_tracker.py
+++ tests/calculators/test_maximum_power_point_tracker.py
@@ -9,6 +9,6 @@ class TestMaximumPowerPointTracker:
         could directly produce 40 W/m^2. (DOI: 10.1021/acsphotonics.9b00679)"""
         max_power = MaximumPowerPointTracker(
             t_cell=443 * u.Kelvin, t_sky=270 * u.Kelvin, E_g=0.17 * u.eV
-        ).get_max_power()
+        ).max_power
         assert round(max_power.value, 1) == 40.8
         assert max_power.unit == (u.watt / (u.meter**2))
