d847f1111b21514fe20d4d94208b56992c4f7d2f
Guy Keogh <guykeogh@proton.me>
2023-03-15 19:44:05 +0000

    Rm unused file, attempt to fix sonarcloud properties

--- src/constants.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from astropy import units as u
-
-
-@dataclass(frozen=True)
-class Characteristics_HgCdZnTe:
-    E_g = 0.218 * u.electronvolt  # bandgap of semiconductor [eV]
